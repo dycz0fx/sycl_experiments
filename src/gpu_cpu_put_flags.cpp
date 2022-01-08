097056492a22113c55058c74b8476d2bcc0f4e8e
Sriraj <sriraj.paul@intel.com>
2022-01-07 16:53:49 -0800

    Break infinte loop due to divergence by eagerly processing any buffer space available. Seems to work correctly and communicate oshmem_put parameters from gpu to cpu while kernel is running.

--- src/gpu_cpu_put_flags.cpp
+++ src/gpu_cpu_put_flags.cpp
@@ -1,4 +1,4 @@
-//dpcpp -g -O0 gpu_cpu_put_flags.cpp -lpthread -lze_loader
+//dpcpp -g gpu_cpu_put_flags.cpp -lpthread -lze_loader
 //srun -n 1 ./a.out
 
 #include<CL/sycl.hpp>
@@ -12,7 +12,7 @@
 #define VLT volatile
 
 constexpr size_t N = 32;
-constexpr size_t T = 128;
+constexpr size_t T = 1024;
 constexpr size_t capacity = 64;
 
 struct packet_t {
@@ -29,6 +29,9 @@ struct info_t {
 
     //DEBUG START
     size_t * tmp_counter;
+    size_t * tmp_index;
+    size_t * tmp_if;
+    size_t * tmp_while;
     size_t * flag_tmp1;
     size_t * flag_tmp2;
     size_t * flag_tmp3;
@@ -37,27 +40,8 @@ struct info_t {
 
 #define cpu_relax() asm volatile("rep; nop")
 
-void oshmem_int64_t_put(int64_t dest, int64_t val, info_t info) {
-    packet_t pkt {dest, val, 1};
-    
-    //find an index to put the packet
-    sycl::ext::oneapi::atomic_ref<size_t, sycl::memory_order::acq_rel, sycl::memory_scope::device, sycl::access::address_space::ext_intel_global_device_space> last_atomic(info.last[0]);
-    size_t index = last_atomic.fetch_add(1);
 
-    //DEBUG START
-    auto idx = sycl::ext::oneapi::experimental::this_nd_item<1>();
-    int g_idx = idx.get_global_id(0);
-    sycl::ext::oneapi::atomic_ref<size_t, sycl::memory_order::acq_rel, sycl::memory_scope::device, sycl::access::address_space::ext_intel_global_device_space> tmp_counter_atomic(info.tmp_counter[g_idx]);
-    tmp_counter_atomic.fetch_add(g_idx);
-    //DEBUG END
-
-    //make sure there is space
-    //if there is no space, wait until space becomes available
-    sycl::ext::oneapi::atomic_ref<size_t, sycl::memory_order::acq_rel, sycl::memory_scope::device, sycl::access::address_space::ext_intel_global_device_space> first_atomic(info.first[0]);
-    //while((int64_t)(first_atomic.load()) <= first_required);
-    assert(index >= first_atomic.load());
-    while(index - first_atomic.load() >= capacity) {tmp_counter_atomic++;}
-    tmp_counter_atomic++;
+void add_data_to_buffer(packet_t pkt, size_t index, info_t info, sycl::ext::oneapi::atomic_ref<size_t, sycl::memory_order::acq_rel, sycl::memory_scope::device, sycl::access::address_space::ext_intel_global_device_space> tmp_counter_atomic) {
 
     //write data to the buffer
     //sycl::ext::oneapi::atomic_ref<size_t, sycl::memory_order::acq_rel, sycl::memory_scope::device, sycl::access::address_space::ext_intel_global_device_space> buff_atomic(info.buff[index%capacity]);
@@ -73,6 +57,9 @@ void oshmem_int64_t_put(int64_t dest, int64_t val, info_t info) {
         buff_atomic.store(pkt_ptr[i]);
     }
 
+    //DEBUG
+    tmp_counter_atomic++;
+
     //sycl::ext::oneapi::atomic_ref<int64_t, sycl::memory_order::acq_rel, sycl::memory_scope::device, sycl::access::address_space::ext_intel_global_device_space> buff_atomic(info.buff[0].flag);
     //buff_atomic.store(1);
     
@@ -80,10 +67,55 @@ void oshmem_int64_t_put(int64_t dest, int64_t val, info_t info) {
     sycl::ext::oneapi::atomic_ref<size_t, sycl::memory_order::acq_rel, sycl::memory_scope::device, sycl::access::address_space::ext_intel_global_device_space> flag_atomic(info.flag[index%capacity]);
     flag_atomic.store(1);
 
+    //DEBUG
+    tmp_counter_atomic++;
+}
+
+void oshmem_int64_t_put(int64_t dest, int64_t val, info_t info) {
+    packet_t pkt {dest, val, 1};
+    
+    //find an index to put the packet
+    sycl::ext::oneapi::atomic_ref<size_t, sycl::memory_order::acq_rel, sycl::memory_scope::device, sycl::access::address_space::ext_intel_global_device_space> last_atomic(info.last[0]);
+    size_t index = last_atomic.fetch_add(1);
+
     //DEBUG START
-    info.flag_tmp1[index%capacity] = flag_atomic.load();
-    info.flag_tmp2[index%capacity] = info.flag[index%capacity];
-    info.flag_tmp3[index%capacity] = index%capacity;
+    auto idx = sycl::ext::oneapi::experimental::this_nd_item<1>();
+    size_t g_idx = idx.get_global_id(0);
+    sycl::ext::oneapi::atomic_ref<size_t, sycl::memory_order::acq_rel, sycl::memory_scope::device, sycl::access::address_space::ext_intel_global_device_space> tmp_index_atomic(info.tmp_index[g_idx]);
+    tmp_index_atomic.store(index);
+    //info.tmp_index[g_idx] = index;
+
+    sycl::ext::oneapi::atomic_ref<size_t, sycl::memory_order::acq_rel, sycl::memory_scope::device, sycl::access::address_space::ext_intel_global_device_space> tmp_counter_atomic(info.tmp_counter[g_idx]);
+    tmp_counter_atomic.store(index);
+    //DEBUG END
+
+    //make sure there is space
+    //if there is no space, wait until space becomes available
+    sycl::ext::oneapi::atomic_ref<size_t, sycl::memory_order::acq_rel, sycl::memory_scope::device, sycl::access::address_space::ext_intel_global_device_space> first_atomic(info.first[0]);
+
+    size_t capacity_req = index - first_atomic.load();
+    if(capacity_req < capacity) {
+        add_data_to_buffer(pkt, index, info, tmp_counter_atomic);
+    }
+
+    //DEBUG
+    info.tmp_if[g_idx] = g_idx;
+
+    assert(index >= first_atomic.load());
+    while(capacity_req >= capacity) { tmp_counter_atomic++;
+        capacity_req = index - first_atomic.load();
+        if(capacity_req < capacity) {
+            add_data_to_buffer(pkt, index, info, tmp_counter_atomic);
+        }
+    }
+
+    //DEBUG START
+    info.tmp_while[g_idx] = 1;
+    tmp_counter_atomic++;
+
+    //info.flag_tmp1[index%capacity] = flag_atomic.load();
+    //info.flag_tmp2[index%capacity] = info.flag[index%capacity];
+    //info.flag_tmp3[index%capacity] = index%capacity;
     //DEBUG END
 }
 
@@ -109,14 +141,18 @@ int main() {
     std::cout<<"selected device : "<<Q.get_device().get_info<sycl::info::device::name>() <<"\n";
     std::cout<<"device vendor : "<<Q.get_device().get_info<sycl::info::device::vendor>() <<"\n";
 
-    size_t size = sizeof(size_t)*(1+1) + sizeof(packet_t)*capacity + sizeof(size_t)*capacity + sizeof(size_t)*capacity;
-    size_t size_full = size + sizeof(size_t)*capacity*3;
+    size_t size = sizeof(size_t)*(1+1) + sizeof(packet_t)*capacity + sizeof(size_t)*capacity;
+    size_t size_tmp = sizeof(size_t)*T*4;
+    size_t size_full = size + size_tmp + sizeof(size_t)*capacity*3;
     VLT size_t * last = sycl::malloc_device<size_t>(size_full, Q);
     VLT size_t * first = last+1;
     packet_t * buff = (packet_t *)(first+1);
     VLT size_t * flag = (size_t *)(buff + capacity);
-    VLT size_t * tmp_counter =  (size_t *)(flag + capacity);
-    size_t * flag_tmp1 = (size_t*)(tmp_counter + capacity);
+    VLT size_t * tmp_counter = (size_t *)(flag + capacity);
+    VLT size_t * tmp_index = (size_t *)(tmp_counter + T);
+    VLT size_t * tmp_if = (size_t *)(tmp_index + T);
+    VLT size_t * tmp_while = (size_t *)(tmp_if + T);
+    size_t * flag_tmp1 = (size_t*)(tmp_while + T);
     size_t * flag_tmp2 = flag_tmp1 + capacity;
     size_t * flag_tmp3 = flag_tmp2 + capacity;
 
@@ -145,11 +181,11 @@ int main() {
     #endif
     Q.wait();
 
-    info_t info{(size_t*)last, (size_t*)first, buff, (size_t*)flag, (size_t*)tmp_counter, flag_tmp1, flag_tmp2, flag_tmp3};
+    info_t info{(size_t*)last, (size_t*)first, buff, (size_t*)flag, (size_t*)tmp_counter, (size_t*)tmp_index, (size_t*)tmp_if, (size_t*)tmp_while, flag_tmp1, flag_tmp2, flag_tmp3};
 
     //create mmap for first and buff
     //size_t *last_mmap = get_mmap_address(last, sizeof(size_t)*1, Q);
-    VLT size_t *last_mmap = get_mmap_address((size_t*)last, size, Q);
+    VLT size_t *last_mmap = get_mmap_address((size_t*)last, size_full, Q);
     //size_t *first_mmap = get_mmap_address(first, sizeof(size_t)*1, Q);
     VLT size_t *first_mmap = last_mmap+1;
 
@@ -158,8 +194,11 @@ int main() {
     //size_t *flag_mmap = get_mmap_address(flag, sizeof(size_t)*capacity, Q);
     VLT size_t *flag_mmap = (size_t *)(buff_mmap+capacity);
     VLT size_t *tmp_counter_mmap = (size_t *)(flag_mmap+capacity);
+    VLT size_t *tmp_index_mmap = (size_t *)(tmp_counter_mmap+T);
+    VLT size_t *tmp_if_mmap = (size_t *)(tmp_index_mmap+T);
+    VLT size_t *tmp_while_mmap = (size_t *)(tmp_if_mmap+T);
 
-    std::cout<<"("<<first_mmap<<" "<<first_mmap<<") ("<<last<<" "<<last_mmap<<")\n";
+    //std::cout<<"("<<first_mmap<<" "<<first_mmap<<") ("<<last<<" "<<last_mmap<<")\n";
 
     std::thread host_thread = std::thread([=]() {
     //auto e_h = Q.submit([=](sycl::handler &h) {
@@ -181,10 +220,13 @@ int main() {
                     count_tmp++;
                     if(count_tmp%10000 == 0) {
                         count_tmp = 0;
-                        std::string print_str = "";
+                        std::string print_str = "host thread mid: ";
                         for(int i=0;i<T;i++) {
-                            print_str.append(std::to_string(tmp_counter_mmap[i]));
-                            print_str.append(" ");
+                            print_str.append("("+std::to_string(tmp_index_mmap[i])+",");
+                            print_str.append(std::to_string(tmp_counter_mmap[i])+",");
+                            print_str.append(std::to_string(tmp_if_mmap[i])+",");
+                            print_str.append(std::to_string(tmp_while_mmap[i])+")");
+                            //print_str.append(" ");
                         }
                         print_str.append("\n");
                         std::cout<<print_str;
@@ -237,6 +279,18 @@ int main() {
     fflush(stdout);
 
     //DEBUG START
+    std::string print_str = "(index,counter,if,while): ";
+    for(int i=0;i<T;i++) {
+        print_str.append("("+std::to_string(tmp_index_mmap[i])+",");
+        print_str.append(std::to_string(tmp_counter_mmap[i])+",");
+        print_str.append(std::to_string(tmp_if_mmap[i])+",");
+        print_str.append(std::to_string(tmp_while_mmap[i])+")");
+        print_str.append(" ");
+    }
+    print_str.append("\n");
+    std::cout<<print_str;
+    fflush(stdout);
+
     #if 0
     for(int i=0;i<capacity;i++)
         std::cout<<flag_mmap[i]<<" ";
