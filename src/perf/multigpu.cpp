5915ab8f38149e18bc5fd7cca177b2d694f3f36f
Stewart, Lawrence <lawrence.stewart@intel.com>
2022-10-28 11:15:15 -0700

    first bw test

--- src/perf/multigpu.cpp
+++ src/perf/multigpu.cpp
@@ -10,7 +10,8 @@
 #include <sys/stat.h>
 #include <immintrin.h>
 
-constexpr size_t BUFSIZE = (65536);
+constexpr size_t BUFSIZE = (1<<27);
+constexpr size_t CHECKSIZE = 4096;
 constexpr double NSEC_IN_SEC = 1000000000.0;
 
 
@@ -80,27 +81,23 @@ void printduration(const char* name, sycl::event e)
     std::cout << name << " execution time: " << duration << " sec" << std::endl;
   }
 
-char checkbuf[BUFSIZE];
+char checkbuf[CHECKSIZE];
 
 void memcheck(const char *msg, int src, int dest, int val, size_t size) {
   unsigned errors = 0;
   for (int i = 0; i < size; i += 1) {
     if (checkbuf[i] != val) errors += 1;
-    if (errors == 1) printf ("expected %d got %d\n", val, checkbuf[i]);
+    if (errors == 1) printf ("    offset %d expected %d got %d\n", i, val, checkbuf[i]);
   }
-  if (errors != 0) printf("%s %d to %d val %d errors %d\n", msg,  src, dest, val, errors);
+  if (errors != 0) printf("    %s %d to %d val %d errors %d\n", msg,  src, dest, val, errors);
 }
 
-void setandcopyback(sycl::queue q, uint64_t *p, int val, size_t size) {
-  auto e = q.submit([&](sycl::handler &h) {
-      h.memset(p, val, BUFSIZE);
-    });
-  e.wait_and_throw();
-  
-  e = q.submit([&](sycl::handler &h) {
-      h.memcpy(&checkbuf[0], p, BUFSIZE);
-    });
-  e.wait_and_throw();
+void setandcheck(const char *msg, int src, int dest, sycl::queue q, uint64_t *p, int val, size_t size) {
+  q.memset(p, val, size);
+  q.wait_and_throw();
+  q.memcpy(checkbuf, p, size);
+  q.wait_and_throw();
+  memcheck(msg, src, dest, val, size);
 }
 
 // ############################
@@ -133,82 +130,48 @@ int main(int argc, char *argv[]) {
   int haz_ngpu = qs.size();
   std::cout << "Number of GPUs found  = " << haz_ngpu << std::endl;
 
-  int ngpu = 6;   // make a command line argument
+  int ngpu = 2;   // make a command line argument
   
-  std::vector<uint64_t *> host_mem(ngpu);
   std::vector<uint64_t *> device_src(ngpu);
-  std::vector<uint64_t *> host_view_device_src(ngpu);
   std::vector<uint64_t *> device_dest(ngpu);
-  std::vector<uint64_t *> host_view_device_dest(ngpu);
-  std::vector<uint64_t *> device_src_map(ngpu);
-  std::vector<uint64_t *> device_dest_map(ngpu);
+
   // vector of a vector of pointers
-  std::vector<uint64_t **> host_copy_device_src_map(ngpu);
-  std::vector<uint64_t **> host_copy_device_dest_map(ngpu);
+  std::vector<uint64_t **> device_src_map(ngpu);
+  std::vector<uint64_t **> device_dest_map(ngpu);
 
   for (int i = 0; i < ngpu; i += 1) {
-    host_mem[i] = (uint64_t *) sycl::aligned_alloc_host(4096, BUFSIZE, qs[i]);
     device_src[i] = (uint64_t *) sycl::aligned_alloc_device(4096, BUFSIZE, qs[i]);
-    host_view_device_src[i]= get_mmap_address(device_src[i], BUFSIZE, qs[i]);
-    device_src_map[i] = (uint64_t *) sycl::aligned_alloc_device(4096, ngpu * sizeof(uint64_t *), qs[i]);
     device_dest[i] = (uint64_t *) sycl::aligned_alloc_device(4096, BUFSIZE, qs[i]);
-    host_view_device_dest[i]= get_mmap_address(device_dest[i], BUFSIZE, qs[i]);
-
-    device_dest_map[i] = (uint64_t *) sycl::aligned_alloc_device(4096, ngpu * sizeof(uint64_t *), qs[i]);
-    host_copy_device_src_map[i] = (uint64_t **) sycl::aligned_alloc_host(4096, ngpu * sizeof(uint64_t *), qs[i]);
+    device_src_map[i] = (uint64_t **) sycl::aligned_alloc_host(4096, ngpu * sizeof(uint64_t *), qs[i]);
     
-    host_copy_device_dest_map[i] = (uint64_t **) sycl::aligned_alloc_host(4096, ngpu * sizeof(uint64_t *), qs[i]);
+    device_dest_map[i] = (uint64_t **) sycl::aligned_alloc_host(4096, ngpu * sizeof(uint64_t *), qs[i]);
 
-    std::cout << " host_mem[" << i << "] " << host_mem[i] << std::endl;
     std::cout << " device_src[" << i << "] " << device_src[i] << std::endl;
-    std::cout << " host_view_device_src[" << i << "] " << host_view_device_src[i] << std::endl;
     std::cout << " device_dest[" << i << "] " << device_dest[i] << std::endl;
-    std::cout << " host_view_device_dest[" << i << "] " << host_view_device_dest[i] << std::endl;
   }
 
+  #if 0
   // initialize IPC addresses for each GPU
   for (int i = 0; i < ngpu; i += 1) {
     for (int j = 0; j < ngpu; j += 1) {
       if (i == j) {
-	host_copy_device_src_map[j][i] = device_src[i];
-	host_copy_device_dest_map[j][i] = device_dest[i];
+	device_src_map[j][i] = device_src[i];
+	device_dest_map[j][i] = device_dest[i];
       } else {
-	host_copy_device_src_map[j][i] = get_ipc_address(device_src[i], BUFSIZE, qs[i],qs[j]);
-	host_copy_device_dest_map[j][i] = get_ipc_address(device_dest[i], BUFSIZE, qs[i],qs[j]);
+	device_src_map[j][i] = get_ipc_address(device_src[i], BUFSIZE, qs[i],qs[j]);
+	device_dest_map[j][i] = get_ipc_address(device_dest[i], BUFSIZE, qs[i],qs[j]);
       }
     }
   }
   for (int i = 0; i < ngpu; i += 1) {
     for (int j = 0; j < ngpu; j += 1) {
-      std::cout << "ipc src view from " << i << " of " << j << ": " << host_copy_device_src_map[i][j] << std::endl; 
+      std::cout << "ipc src view from " << i << " of " << j << ": " << device_src_map[i][j] << std::endl; 
     }
     for (int j = 0; j < ngpu; j += 1) {
-      std::cout << "ipc dest view from " << i << " of " << j << ": " << host_copy_device_dest_map[i][j] << std::endl; 
+      std::cout << "ipc dest view from " << i << " of " << j << ": " << device_dest_map[i][j] << std::endl; 
     }
   }
-    
-
-
-
   
-  // initialize memories
-  for (int i = 0; i < ngpu; i += 1) {
-      memset(host_mem[i], 28, BUFSIZE);
-      auto e = qs[i].submit([&](sycl::handler &h) {
-	  h.memset(device_src[i], i, BUFSIZE);
-	});
-      e.wait_and_throw();
-      #if 0
-      e = qs[i].submit([&](sycl::handler &h) {
-	  h.memcpy(&device_src_map[i][0], &host_copy_device_src_map[i][0], ngpu * sizeof(uint64_t *));
-	});
-      e.wait_and_throw();
-      e = qs[i].submit([&](sycl::handler &h) {
-	  h.memcpy(&device_dest_map[i][0], &host_copy_device_dest_map[i][0], ngpu * sizeof(uint64_t *));
-	});
-      e.wait_and_throw();
-      #endif
-  }
   
   // initialize device_src
   std::cout << " memory initialized " << std::endl;
@@ -231,19 +194,14 @@ int main(int argc, char *argv[]) {
   // from i to j
   for (int i = 0; i < ngpu; i += 1) {
     for (int j = 0; j < ngpu; j += 1) {
-      setandcopyback(qs[i], &host_copy_device_src_map[i][i][0], i, BUFSIZE);
-      memcheck("store source", i, j, i, BUFSIZE);
-      setandcopyback(qs[j], &host_copy_device_dest_map[j][j][0], i, BUFSIZE);
-      memcheck("store dest", i, j, i, BUFSIZE);
-      auto e = qs[i].submit([&](sycl::handler &h) {
-	  h.memcpy(&host_copy_device_dest_map[i][j][0], &host_copy_device_src_map[i][i][0], BUFSIZE);
-	});
-      e.wait_and_throw();
-      e = qs[j].submit([&](sycl::handler &h) {
-	  h.memcpy(checkbuf, &host_copy_device_dest_map[j][j][0], BUFSIZE);
-	});
-      e.wait_and_throw();
-      memcheck("store", i, j, i, BUFSIZE);
+      std::cout << "store from " << i << " to " << j << std::endl;
+      setandcheck("  store source", i, j, qs[i], &device_src_map[i][i][0], i, CHECKSIZE);
+      setandcheck("  store dest", i, j, qs[j], &device_dest_map[j][j][0], 100 + j, CHECKSIZE);
+      qs[i].memcpy(&device_dest_map[i][j][0], &device_src_map[i][i][0], CHECKSIZE);
+      qs[i].wait_and_throw();
+      qs[j].memcpy(checkbuf, &device_dest_map[j][j][0], CHECKSIZE);
+      qs[j].wait_and_throw();
+      memcheck("  store", i, j, i, CHECKSIZE);
     }
   }
 
@@ -253,24 +211,46 @@ int main(int argc, char *argv[]) {
   // from i to j
   for (int i = 0; i < ngpu; i += 1) {
     for (int j = 0; j < ngpu; j += 1) {
-      setandcopyback(qs[i], &host_copy_device_src_map[i][i][0], i, BUFSIZE);
-      memcheck("load src", i, -1, i, BUFSIZE);
-      setandcopyback(qs[j], &host_copy_device_dest_map[j][j][0], 100 + i, BUFSIZE);
-      memcheck("load dest", i, -1, 100 + i, BUFSIZE);
-      auto e = qs[j].submit([&](sycl::handler &h) {
-	  h.memcpy(&host_copy_device_dest_map[j][j][0], &host_copy_device_src_map[j][i][0], BUFSIZE);
-	});
-      e.wait_and_throw();
-      e = qs[j].submit([&](sycl::handler &h) {
-	  h.memcpy(checkbuf, &host_copy_device_dest_map[j][j][0], BUFSIZE);
-	});
-      e.wait_and_throw();
-      memcheck("load ", i, j, i, BUFSIZE);
+      std::cout << "load from " << i << " to " << j << std::endl;
+      setandcheck("  load src", i, j, qs[i], &device_src_map[i][i][0], i, CHECKSIZE);
+      setandcheck("  load dest", i, j, qs[j], &device_dest_map[j][j][0], 100 + j, CHECKSIZE);
+      qs[j].memcpy(&device_dest_map[j][j][0], &device_src_map[j][i][0], CHECKSIZE);
+      qs[j].wait_and_throw();
+      qs[j].memcpy(checkbuf, &device_dest_map[j][j][0], CHECKSIZE);
+      qs[j].wait_and_throw();
+      memcheck("  load ", i, j, i, CHECKSIZE);
     }
   }
 
+  std::cout<<"test readback map" << std::endl;
+  for (int i = 0; i < ngpu; i += 1) {
+    for (int j = 0; j < ngpu; j += 1) {
+      std::cout << "readback map on " << i << " to " << j << std::endl;
+      setandcheck(" readback map ", i, j, qs[i], device_src_map[i][j], (i << 4) + j, CHECKSIZE);
+    }
+  }
+  
+#endif
+  std::cout << "test readback no ipc" << std::endl;
+  for (int i = 0; i < ngpu; i += 1) {
+    for (int j = 0; j < ngpu; j += 1) {
+      std::cout << "readback on " << i << " to " << j << std::endl;
+      setandcheck(" readback ", i, j, qs[i], device_src[j], (i << 4) + j, CHECKSIZE);
+
+    }
+  }
+  std::cout << "test readback dest no ipc" << std::endl;
+  for (int i = 0; i < ngpu; i += 1) {
+    for (int j = 0; j < ngpu; j += 1) {
+      std::cout << "readback dest on " << i << " to " << j << std::endl;
+      setandcheck(" readback dest ", i, j, qs[i], device_dest[j], (i << 4) + j, CHECKSIZE);
+
+    }
+  }
+  
 
   // try without the maps
+#define TRYNOMAPS 1
 #if TRYNOMAPS
   std::cout<<"store without ipc maps" << std::endl;
 
@@ -278,19 +258,17 @@ int main(int argc, char *argv[]) {
   // from i to j
   for (int i = 0; i < ngpu; i += 1) {
     for (int j = 0; j < ngpu; j += 1) {
-      setandcopyback(qs[i], &device_src[i][0], i, BUFSIZE);
-      memcheck("store source", i, j, i, BUFSIZE);
-      setandcopyback(qs[j], &device_dest[j][0], j + 100, BUFSIZE);
-      memcheck("store dest", i, j, j + 100, BUFSIZE);
-      auto e = qs[i].submit([&](sycl::handler &h) {
-	  h.memcpy(&device_dest[j][0], &device_src[i][0], BUFSIZE);
-	});
-      e.wait_and_throw();
-      e = qs[j].submit([&](sycl::handler &h) {
-	  h.memcpy(checkbuf, &device_dest[j][0], BUFSIZE);
-	});
-      e.wait_and_throw();
-      memcheck("store", i, j, i, BUFSIZE);
+      std::cout << "store from " << i << " to " << j << std::endl;
+      
+      //assert(device_src[i] == device_src_map[i][i]);
+      //assert(device_dest[j] == device_dest_map[j][j]);
+      setandcheck("  store source", i, j, qs[i], &device_src[i][0], i, CHECKSIZE);
+      setandcheck("  store dest", i, j, qs[j], &device_dest[j][0], j + 100, CHECKSIZE);
+      qs[i].memcpy(&device_dest[j][0], &device_src[i][0], CHECKSIZE);
+      qs[i].wait_and_throw();
+      qs[j].memcpy(checkbuf, &device_dest[j][0], CHECKSIZE);
+      qs[j].wait_and_throw();
+      memcheck("  store", i, j, i, CHECKSIZE);
     }
   }
 
@@ -299,26 +277,78 @@ int main(int argc, char *argv[]) {
   // from i to j
   for (int i = 0; i < ngpu; i += 1) {
     for (int j = 0; j < ngpu; j += 1) {
-      setandcopyback(qs[i], &device_src[i][0], i, BUFSIZE);
-      memcheck("load src", i, -1, i, BUFSIZE);
-      setandcopyback(qs[j], &device_dest[j][0], 100 + j, BUFSIZE);
-      memcheck("load dest", i, -1, 100 + j, BUFSIZE);
-      auto e = qs[j].submit([&](sycl::handler &h) {
-	  h.memcpy(&device_dest[j][0], &device_src[i][0], BUFSIZE);
-	});
-      e.wait_and_throw();
-      e = qs[j].submit([&](sycl::handler &h) {
-	  h.memcpy(checkbuf, &device_dest[j][0], BUFSIZE);
-	});
-      e.wait_and_throw();
-      memcheck("load ", i, j, i, BUFSIZE);
+      std::cout << "load from " << i << " to " << j << std::endl;
+      //assert(device_src[i] == device_src_map[i][i]);
+      //assert(device_dest[j] == device_dest_map[j][j]);
+      setandcheck("  load src", i, j, qs[i], &device_src[i][0], i, CHECKSIZE);
+      setandcheck("  load dest", i, j, qs[j], &device_dest[j][0], 100 + j, CHECKSIZE);
+      qs[j].memcpy(&device_dest[j][0], &device_src[i][0], CHECKSIZE);
+      qs[j].wait_and_throw();
+      qs[j].memcpy(checkbuf, &device_dest[j][0], CHECKSIZE);
+      qs[j].wait_and_throw();
+      memcheck("  load ", i, j, i, CHECKSIZE);
     }
   }
 #endif
-
+  // size is in bytes
+  for (size_t size = 8; size < BUFSIZE; size <<= 1) {
+    int maxthreads = size/8;
+    if (maxthreads > 1024) maxthreads = 1024;
+    for (int threads = 1; threads <= maxthreads; threads <<= 1) {
+      // locloop is in uint64_t
+      uint64_t loc_loop = (size/8) / threads;
+      double duration;
+      int count;
+      // run for more and more counts until it takes more than 0.1 sec
+      for (count = 1; count < (1 << 20); count <<= 1) {
+	uint64_t *s = device_src[1];
+	uint64_t *d = device_dest[0];
+	clock_gettime(CLOCK_REALTIME, &ts_start);
+	auto e = qs[0].submit([&](sycl::handler &h) {
+	    h.parallel_for_work_group(sycl::range(1), sycl::range(threads), [=](sycl::group<1> grp) {
+		grp.parallel_for_work_item([&] (sycl::h_item<1> it) {
+		    int j = it.get_global_id()[0];
+		    for (int iter = 0; iter < count; iter += 1) {
+		      for (int k = j * loc_loop; k < (j+1) * loc_loop; k += 1)
+			d[k] = s[k];
+		    }
+		  });
+		sycl::atomic_fence(sycl::memory_order::acquire, sycl::memory_scope::system);
+	      });
+	  });
+	e.wait_and_throw();
+	clock_gettime(CLOCK_REALTIME, &ts_end);
+	duration = ((double) (ts_end.tv_sec - ts_start.tv_sec)) * 1000000000.0 +
+	  ((double) (ts_end.tv_nsec - ts_start.tv_nsec));
+	duration /= 1000000000.0;
+	if (duration > 0.1) {
+	  break;
+	}
+      }
+      double per_iter = duration / count;
+      double bw = size / (per_iter);
+      double bw_mb = bw / 1000000.0;
+      printf("size %ld threads %d count %d duration %f bandwidth %f MB/s\n", size, threads, count, per_iter, bw_mb);
+    }
+  }
   
+      
   std::cout<<"kernel returned" << std::endl;
   return 0;
 
 }
+
+
+
+#if 0
+	uint64_t start =
+	  e.get_profiling_info<sycl::info::event_profiling::command_start>();
+	uint64_t end =
+	  e.get_profiling_info<sycl::info::event_profiling::command_end>();
+	duration = static_cast<double>(end - start) / NSEC_IN_SEC;
+	if (duration > 0.1) {
+	  duration /= count;
+	  break;
+	}
+#endif
 
