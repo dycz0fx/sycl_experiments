25b19fa004803683520da29e04098da93079f1cb
Stewart, Lawrence <lawrence.stewart@intel.com>
2023-01-11 09:02:29 -0800

    fix memory location

--- src/perf/dohr2cp0.sh
+++ src/perf/dohr2cp0.sh
@@ -1,9 +1,9 @@
 for bthreads in 1 2 4 8 16 32; do
-    srun src/perf/helperring2 --a=c --b=p0 --a2bbuf=p0 --b2abuf=t0 --athreads=1 --bthreads=${bthreads} --a2bcount=500000 --b2acount=0
+    srun src/perf/helperring2 --a=c --b=p0 --a2bbuf=p0 --b2abuf=a --athreads=1 --bthreads=${bthreads} --a2bcount=500000 --b2acount=0
 done
 for bthreads in 1 2 4 8 16 32; do
-    srun src/perf/helperring2 --a=c --b=p0 --a2bbuf=p0 --b2abuf=t0 --athreads=1 --bthreads=${bthreads} --a2bcount=0 --b2acount=500000
+    srun src/perf/helperring2 --a=c --b=p0 --a2bbuf=p0 --b2abuf=a --athreads=1 --bthreads=${bthreads} --a2bcount=0 --b2acount=500000
 done
 for bthreads in 1 2 4 8 16 32; do
-    srun src/perf/helperring2 --a=c --b=p0 --a2bbuf=p0 --b2abuf=t0 --athreads=1 --bthreads=${bthreads} --a2bcount=500000 --b2acount=500000
+    srun src/perf/helperring2 --a=c --b=p0 --a2bbuf=p0 --b2abuf=a --athreads=1 --bthreads=${bthreads} --a2bcount=500000 --b2acount=500000
 done
