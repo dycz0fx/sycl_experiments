e8acfcd3900fc6d9e891ab98d892b25c362ae516
Xi Luo <xi.luo@intel.com>
2025-04-09 15:58:46 -0500

    add bw and bug fixes

--- src/sycl_l0_xelink/gpu_copy_kernel_sycl.cpp
+++ src/sycl_l0_xelink/gpu_copy_kernel_sycl.cpp
@@ -158,9 +158,10 @@ int main(int argc, char **argv) {
     }
 
     auto end = std::chrono::high_resolution_clock::now();
-
-    std::cout << "size : "<<sizeof(long) * N;
-    std::cout << "  time : " << std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/rep<< " µs\n";
+    auto avg_time = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count()/rep;
+    std::cout << "size : "<< sizeof(long) * N;
+    std::cout << "  time : " << avg_time << " µs";
+    std::cout << " bw : " << sizeof(long) * N / avg_time << " MBPS" << std::endl;
 
     long* host_buff = sycl::malloc_host<long>(N * num_devices, q_vec[0]);
     for (int j=0; j<num_devices; j++) {
